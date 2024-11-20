2bb352da47e75e5f15d8a1239f70b4574687bd71
Himanshu Prajapati <127850749+Himanshu2326@users.noreply.github.com>
2024-11-20 15:42:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -22,9 +22,11 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
+    alert('Error connecting to the database:', err);
     return;
   }
   console.log('Connected to the MySQL database.');
+  alert('Connected to the MySQL database.');
 });
 
 module.exports = connection;
